0ca74e13754829a58d2743d8d38f36d71ff9847a
JacobReeder <jacob.reeder1@gmail.com>
2022-06-19 19:29:12 -0700

    Navigation still not functional

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,62 +2,57 @@ import React from 'react';
 import { capitalizeFirstLetter } from "../../utils/helpers"; 
 
 
-function Navigation() {
+function Navigation(props) {
 
-  /** Example array used to keep the code DRY instead of hardcoding navigation. 
-    Explained in 20.3.3 **/
+    const {
+      categories = [],
+      setCurrentCategory,
+      currentCategory,
+    } = props;
 
-    const categories = [
-  {
-    name: "Project 1:", description: "",
-  },
-  { name: "", description: "" },
-  {
-    name: "",
-    description: "",
-  },
-];
+    const handleClick = (item) => {
+      console.log(item);
+      return item;
+    };
 
-function handleClick() {
-   }
    
-
     return (
 
-       <header>
+       <header classNAme="flex-row px-1">
           <h2>
-    <a href="/">
-      <span role="img"> </span> React Portfolio
-    </a>
-  </h2>
-  <nav>
-    <ul className="flex-row">
-      <li className="mx-2">
-        <a href="#about" onClick={() => handleClick()}>
-          About Me          
-        </a>
-      </li>
-      {/**Add Portfolio page link here */}
-      <li className={"mx-2"}>
-        <span onClick={() => handleClick()}>
-          Contact
-        </span>
-      </li>
-      {categories.map((category) => (
-        <li
-          className="mx-1"
-          key={category.name}
-        >
-        {/**May need to refactor every "category" variable//categoyry ties in with each project on project page? */} 
-          <span onClick={() => { handleClick(); }}>
-            {capitalizeFirstLetter(category.name)}       
-         </span>
-        </li>
-        ))
-      }
-   </ul>
-  </nav>
-  </header>
+            <a data-testid="link" href="/">
+              <span role="img"></span> 
+              React Portfolio
+            </a>
+          </h2>
+       <nav>
+         <ul className="flex-row">
+           <li className="mx-2">
+            <a data-testid="about" href="#about">
+             About Me          
+            </a>
+            </li>              
+             <li className={"mx-2"}>
+                <span onClick={() => handleClick('Projects')}>
+                  Projects         
+                </span>
+             </li>
+               {categories.map((category) => (
+                <li className={`mx-1 ${
+                  currentCategory.name === category.name
+                }`} key={category.name}>
+                <span
+                  onClick={() => {
+                      setCurrentCategory(category);
+                  }}
+                >
+                  {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
 
