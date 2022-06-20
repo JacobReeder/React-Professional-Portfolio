58ffbf2430e7302af654f4147b4682d4cfcdb819
JacobReeder <jacob.reeder1@gmail.com>
2022-06-19 17:07:08 -0700

    Added component imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
 
+import About from './components/About';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
-import Project from './components/Project'
+import Projects from './components/Project';
+import Resume from './components/Resume'
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
       <main>
-        <Project></Project>
+        <About></About>
+        <Projects></Projects>
+        <Resume></Resume>
+        <Contact></Contact>
       </main>
     </div>
   );
