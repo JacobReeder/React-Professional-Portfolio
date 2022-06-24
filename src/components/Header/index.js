16c6d48d39af081b7ffacbfddee04978e1584608
JacobReeder <jacob.reeder1@gmail.com>
2022-06-24 16:16:44 -0700

    Updated font size and color in Header styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,3 @@
-import React from 'react'; 
 
-function Header() {
 
-    return (
 
-       <header>
-        <h1>Jacob Reeder</h1>
-        {/*props.children*/}
-       </header>
-       
-    );
-}
-
-export default Header;
