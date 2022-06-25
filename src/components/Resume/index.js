c8cc9b30ae3212c9b7ad12adc471126b71b2c7e4
JacobReeder <jacob.reeder1@gmail.com>
2022-06-24 19:04:50 -0700

    Added Resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,21 @@ function Resume() {
 
         <section>
             <h1 style={{ backgroundImage: `url(${background})` }}>Resume</h1>
-            <p></p>
+             <ul>
+                 <li>Relevant Experience: </li>
+                 <li>  Full Stack Web Development Certification from UCB Berkely in 2022.</li>     
+            </ul> 
+            <ul>
+                 <li>Proficient in the Following Coding Languages:</li>
+                 <li>Html, CSS, Javascript, JSX </li>
+            </ul>
+            <ul>
+                 <li>Also proficient with:</li>
+                 <li>Third party API's ex. Bootstrap, JQUERY; Object-Oriented Programming, ORM, MVC; </li>
+                 <li>Node.js and various packages such as (but not limited to) Express.js, Jest testing framework, Handlebars;</li>
+                 <li>Various Databases such as SQL, NOSQL, and Mongoose;</li>
+                 <li>as well as Testing Frameworks such as Insomnia, Heroku, and React.</li>
+            </ul>
         </section>
     );
 }
