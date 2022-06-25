b44797bde51290ba4d8c54939b3e01b420252c80
JacobReeder <jacob.reeder1@gmail.com>
2022-06-24 19:03:56 -0700

    Added github link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,13 @@
 import React from 'react'; 
+import githubLogo from "../../assets/Projects/GitHub.png"
 
 function Footer() {
 
     return (
 
-        <footer className="footer" style={{position:"fixed"}}>
-            <p>GitHub Profile
-                <a href="https://github.com/JacobReeder"></a>
+        <footer className="footer" style={{ position:"fixed" }}>
+            <p style={{ backgroundImage: `url(${githubLogo})` }}>
+                <a href="https://github.com/JacobReeder" >GitHub Profile</a>
             </p>
         </footer>
     );
